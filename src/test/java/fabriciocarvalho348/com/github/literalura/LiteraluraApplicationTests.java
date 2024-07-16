5af440cf469f337bffa7a694723e983127769d51
Fabricio Carvalho <fabricioccarvalho348@gmail.com>
2024-07-16 19:16:29 -0300

    feat: Criando API LiterAlura

---
+++ src/test/java/fabriciocarvalho348/com/github/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package fabriciocarvalho348.com.github.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
